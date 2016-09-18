338f8ed6f6fdb302f4ca0b8bc1516058434de461
Kevin Snyder <Kevin Snyder>
2016-09-18 16:46:01 -0700

    Read ICS file from internet; Merge multi-day events; Ignore events that have passed; Format events for jinja2 (Markdown to HTML, start and end times)

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import feedparser
+from urllib.request import urlopen
+from icalendar import Calendar, Event, vDatetime
+from datetime import datetime, date, timezone
 import os
 from markdown2 import Markdown
 import jinja2
 
-d = feedparser.parse('https://api.orgsync.com/api/v3/portals/139259/events.rss?key=TD18DDaXso6j_3R0F_nnpYHU4Xh5XnUaNBh0ZLpB9Ac&per_page=100&upcoming=true')
-for entry in d.entries:
-	print(entry.title)
-	print(entry.link)
-	print(entry.description)
-	print(entry.published)
-	print(entry.published_parsed)
-	print(entry.id)
+def getCal(url):
+	with urlopen(url) as ics:
+		return Calendar.from_ical(ics.read())
 
 def render(tpl_path, context):
 	path, filename = os.path.split(tpl_path)
@@ -18,13 +15,48 @@ def render(tpl_path, context):
 		loader=jinja2.FileSystemLoader(path or './')
 	).get_template(filename).render(context)
 
-context = {
-	'message': '''Hello, world!\n\nTesting, testing.\n\n- Item 1\n- Item 2'''
-}
+url = "https://seattleu.instructure.com/feeds/calendars/user_hDYTHQB70FzLttLqhwsWN8dCxxi8OZkX16aYSCN2.ics"
+cal = getCal(url)
 md = Markdown()
-for key, val in context.items():
-	context[key] = md.convert(val)
-print(context['message'])
+context = {
+	'message': md.convert('''Hello, world!\n\nTesting, testing.\n\n- Item 1\n- Item 2'''),
+	'events': []
+}
+for item in cal.walk():
+	if item.name == "VEVENT" and item.get("summary").endswith(" [CS Club]"):
+		dt = item.get('dtstart').dt
+		if not isinstance(dt, datetime):
+			dt = datetime.combine(dt, datetime.min.time()).replace(tzinfo=timezone.utc)
+		else:
+			dt = dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+		if dt > datetime.now(timezone.utc):
+			event = {
+				'name': item.get('summary')[:-len(" [CS Club]")],
+				'description': md.convert(item.get('description')),
+				'sMonth': dt.strftime('%b'),
+				'sDay': dt.strftime('%d'),
+				'sTime': dt.strftime('%I:%M %p').lstrip('0')
+			}
+			duplicate = False
+			for i in range(len(context['events'])):
+				if context['events'][i]['name'] == event['name'] and context['events'][i]['description'] == event['description']:
+					duplicate = True
+					# No need to compare dates since events are processed in chronological order
+					# (current event will always have greater date than last event)
+					context['events'][i]['eMonth'] = event['sMonth']
+					context['events'][i]['eDay'] = event['sDay']
+					context['events'][i]['eTime'] = event['sTime']
+			if not duplicate:
+				context['events'].append(event)
+			# for k,v in event.items():
+			# 	print(k, v.replace(u"\u2018", "'").replace(u"\u2019", "'"))
+			# print()
+
+for event in context['events']:
+	for k,v in event.items():
+		print(k, v.replace(u"\u2018", "'").replace(u"\u2019", "'"))
+
+print(str(context).replace(u"\u2018", "'").replace(u"\u2019", "'"))
 result = render('template.html', context)
 with open('result.html', 'w') as file:
 	file.write(result)
